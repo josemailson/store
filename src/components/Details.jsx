74fe7b7d15441a10b1b457ed2ba15b8d5a0f3ce5
josemailson <jmailsons@gmail.com>
2023-03-22 22:04:18 -0300

    some details page styles

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { getProduct } from "../services/api"
 import { useLoaderData } from "react-router-dom"
+import "../styles/Details.css"
+import Header from "./Header";
 
 export async function loaderProducts({ params }) {
     const product = await getProduct(params.id);
@@ -11,12 +13,17 @@ export function Details() {
     const { product } = useLoaderData();
     return (
       <div className="details">
+        <Header />
+        <div className="images">
         {product.images.map((image, key) => {
             return <img src={image} alt="" className="imgCard" key={key}/>;
         })}
+        </div>
+        <div className="texts">
         <p>{product.title}</p>
         <p>R$ {(product.price).toFixed(2)}</p>
         <p>{product.description}</p>
+        </div>
       </div>
     );
   }
